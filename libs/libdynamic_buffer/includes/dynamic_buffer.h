d1a969648a25bf9e71735f2810cb7f342c6a45dd
Jeremy Maia <jmaia@student.42.fr>
2022-02-07 17:36:11 +0100

    Make get_elem_ptr public

--- libs/libdynamic_buffer/includes/dynamic_buffer.h
+++ libs/libdynamic_buffer/includes/dynamic_buffer.h
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 11:09:40 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/03 11:17:13 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/02/07 17:35:12 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,5 @@
 # include "libft.h"
 # include "libdynamic_buffer.h"
 
-static void			*get_elem_ptr(t_dynamic_buffer *buffer, unsigned long i);
 static int			expand(t_dynamic_buffer *buffer);
 #endif
